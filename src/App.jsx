6a080dbe61ce711ba9cce0f52a83d59fed1c9d5c
George Uzoh <ugochukwuuzoh@gmail.com>
2023-11-09 08:22:30 +0200

    add functionality to search filter

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,29 @@ import Card from "./Card";
 import { useState } from "react";
 
 function App() {
-  const [wildlife, setWildlife] = useState(animals);
+  const [wildlife, setWildlife] = useState([...animals]);
 
   function deleteItem(name) {
     const newList = wildlife.filter((wildAminal) => wildAminal.name != name);
     setWildlife(newList);
+  }
+
+  function searchItem(e) {
+    const searchText = e.target.value.toLowerCase();
+    const filteredList = wildlife?.filter((word) =>
+      word.name.toLowerCase().includes(searchText)
+    );
+    setWildlife(filteredList);
+  }
+
+  function handleBackspace(e) {
+    if (e.key === "Backspace") {
+      const searchText = e.target.value.toLowerCase().split('').slice(0, -1).join('');
+      const filteredList = animals?.filter((word) =>
+        word.name.toLowerCase().includes(searchText)
+      );
+      setWildlife(filteredList);
+    }
   }
 
   return (
@@ -22,12 +40,13 @@ function App() {
           <input
             type="text"
             placeholder="Search"
-            // value={searchText}
+            onChange={searchItem}
+            onKeyDown={handleBackspace}
           />
           <span className="material-symbols-outlined"> search </span>
         </div>
         <div className="cards">
-          {wildlife.map((animal) => (
+          {wildlife?.map((animal) => (
             <Card
               key={animal.name}
               {...animal}
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,13 @@
+import { animals } from "./animalsList";
+import { birds } from "./animalsList";
+
 const Header = () => {
     return (
         <header>
             <ul>
                 <li><a href="http://">Home</a></li>
-                <li><a href="http://">Animals</a></li>
-                <li><a href="http://">Birds</a></li>
+                <li><a href="http://">Animals ({animals.length})</a></li>
+                <li><a href="http://">Birds ({birds.length})</a></li>
                 <li><a href="http://">About</a></li>
             </ul>
         </header>
