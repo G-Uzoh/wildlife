0c996e5d56c41b4438340b1cebb3aac573e76d91
George Uzoh <ugochukwuuzoh@gmail.com>
2023-12-30 12:20:29 +0200

    fix footer alignment

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -3,7 +3,7 @@ const About = () => {
         <div className="about">
             <h2>About Wildlife</h2>
             <p>Wildlife is a simple web application that displays a wide variety of animals on the Earth, ranging from amphibians, terrestrial, aerial and aquatic animals.</p>
-            <p>Built with ReactJS, it is a school project created in Business College Helsinki.</p>
+            <p className="second-paragraph">Built with ReactJS, it is a school project created in Business College Helsinki.</p>
         </div>
     );
 }
