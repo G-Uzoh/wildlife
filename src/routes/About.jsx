9ff71b5e61ca24bc6611a0b98dd9bc6c1c11ff35
George Uzoh <ugochukwuuzoh@gmail.com>
2023-12-30 11:55:59 +0200

    fix bug in nav menu

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -2,8 +2,8 @@ const About = () => {
     return (
         <div className="about">
             <h2>About Wildlife</h2>
-            <p>Wildlife is a simple application that displays a diverse species of animals based on the user&apos;s choice.</p>
-            <p>Built with ReactJS, it is a school project of Business College Helsinki</p>
+            <p>Wildlife is a simple web application that displays a wide variety of animals on the Earth, ranging from amphibians, terrestrial, aerial and aquatic animals.</p>
+            <p>Built with ReactJS, it is a school project created in Business College Helsinki.</p>
         </div>
     );
 }
